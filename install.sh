86a04ed55ed6a37159e44aa4afdc12576a27cfd2
Jacopo Cavallo <jacopo.cavallo@protonmail.com>
2017-10-14 21:57:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ if [ "$(id -u)" != "0" ]; then
 fi
 mkdir /opt/arpWatchDogDaemon
 cp ./awdd.py /opt/arpWatchDogDaemon/
+chmod 744 /opt/arpWatchDogDaemon/awdd.py
+cp ./uninstall.sh /opt/arpWatchDogDaemon/
+chmod 744 /opt/arpWatchDogDaemon/uninstall.sh
 cp -R ./img /opt/arpWatchDogDaemon/
 cp ./awdd.sh /etc/profile.d
 chmod 744 /etc/profile.d/awdd.sh

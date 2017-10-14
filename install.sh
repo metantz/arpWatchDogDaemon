d33f77ec113c268c062d9b076e42fefcc5eef358
Jacopo Cavallo <jacopo.cavallo@protonmail.com>
2017-10-14 21:31:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,6 @@ fi
 mkdir /opt/arpWatchDogDaemon
 cp ./awdd.py /opt/arpWatchDogDaemon/
 cp -R ./img /opt/arpWatchDogDaemon/
-cp ./awdd.service /etc/systemd/system/
-chmod 664 /etc/systemd/system/awdd.service
-cp ./awdd.sh /usr/local/sbin/
-chmod 744 /usr/local/sbin/awdd.sh
-systemctl daemon-reload
-systemctl enable awdd.service
-systemctl start awdd.service
+cp ./awdd.sh /etc/profile.d
+chmod 744 /etc/profile.d/awdd.sh
+/etc/profile.d/awdd.sh

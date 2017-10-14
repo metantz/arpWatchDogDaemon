217b8ee1f0ec54ab936802fa22d3cb2f1fe7d2d7
Jacopo Cavallo <jacopo.cavallo@protonmail.com>
2017-10-14 18:04:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-if [[ $EUID -ne 0 ]]; then
+if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-
 mkdir /opt/arpWatchDogDaemon
 cp ./awdd.py /opt/arpWatchDogDaemon/
 cp -R ./img /opt/arpWatchDogDaemon/
